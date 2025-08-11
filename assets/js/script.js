163fb0505ae7ce035d98bb33c8ed88ffe68a3316
jeba <jeba64137@gmail.com>
2025-08-11 14:47:10 +0530

    updated all files

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+var offerBar = document.querySelector(".offer-bar")
+
+document.getElementById("offer-close").addEventListener("click",
+
+function(){
+    offerBar.style.display="none"
+}
+)
+
+var sideNavMenu=document.getElementById("side-navbar-activate")
+var sidenavbar = document.querySelector(".side-navbar")
+sideNavMenu.addEventListener("click",function(){
+    sidenavbar.style.marginLeft="0px"
+})
+
+document.getElementById("side-navbar-close").addEventListener("click",()=>{
+    document.querySelector(".side-navbar").style.marginLeft = "-60%"
+})
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); 
+}
+var likebuttons=document.querySelectorAll(".like-button")
+
+likebuttons.forEach((btn)=>{
+    btn.addEventListener("click",function(e){
+        
+        
+        if(e.target.src.indexOf("blackheart")>0)
+        {
+            console.log("jkjk");
+            e.target.src="img/icons/redheart.png"
+        }
+        else{
+            e.target.src="img/icons/blackheart.png"
+        }
+    })
+})
+
+window.addEventListener("scroll",function(){
+    var elements = this.document.querySelectorAll(".initial-scroll-animate")
+    elements.forEach((el)=>{
+        windowHeight = window.innerHeight
+     var elbound = el.getBoundingClientRect()
+   
+
+     console.log(windowHeight)
+     console.log(elbound.top)
+     if(windowHeight>elbound.top-100){
+        console.log("Hi")
+    el.classList.remove("reveal-scroll-animate")
+
+    }
+    
+})
+    
+})
+
+
+
